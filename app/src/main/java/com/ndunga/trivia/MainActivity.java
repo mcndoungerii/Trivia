c71dddf245efa3da504220dc7fd5a15a4b8706cb
mcndoungerii <allenmcndounger@gmail.com>
2021-05-30 18:34:38 +0300

    Java Lambdas

--- app/src/main/java/com/ndunga/trivia/MainActivity.java
+++ app/src/main/java/com/ndunga/trivia/MainActivity.java
@@ -4,20 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
-import android.util.Log;
-
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.ndunga.trivia.controller.AppController;
 import com.ndunga.trivia.data.Repository;
 import com.ndunga.trivia.databinding.ActivityMainBinding;
 import com.ndunga.trivia.model.Question;
 
-import org.json.JSONArray;
-
-import java.lang.reflect.Method;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     private int currentQuestionIndex = 0;
 
-    private Question quest = new Question();
+    private final Question quest = new Question();
 
     List<Question> questionList;
 
@@ -39,16 +29,12 @@ public class MainActivity extends AppCompatActivity {
 
         binding.setQuestion(quest);
 
-        questionList = new Repository().getQuestion( questionArrayList -> {
-
-
-            binding.questionTextView.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
-        });
+        questionList = new Repository().getQuestion( questionArrayList -> binding.questionTextView.setText(questionArrayList.get(currentQuestionIndex).getAnswer()));
 
         binding.buttonNext.setOnClickListener(view -> {
             currentQuestionIndex = (currentQuestionIndex + 1) % questionList.size();
 
-            //% questionList.size() -- helps us to avoid the error ArrayOutofBoundIndex
+            //% questionList.size() -- helps us to avoid the error ArrayOutBoundIndex
 
             updateQuestion();
 
