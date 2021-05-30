07e2158ce53830bfef6d3631e68cebd57cf9db0c
mcndoungerii <allenmcndounger@gmail.com>
2021-05-30 11:47:35 +0300

    Refact Singleton class to have RequestQueue only\n Override onCreate Class within Singleton

--- app/src/main/java/com/ndunga/trivia/MainActivity.java
+++ app/src/main/java/com/ndunga/trivia/MainActivity.java
@@ -3,12 +3,35 @@ package com.ndunga.trivia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.ndunga.trivia.controller.AppController;
+
+import org.json.JSONArray;
+
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
+
+    String url = "https://raw.githubusercontent.com/curiousily/simple-quiz/master/script/statements-data.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                response -> {
+                    Log.d("Data::::",response.toString());
+
+                }, error -> {
+                    Log.d("Error::::",error.toString());
+                });
+
+        AppController.getInstance().addToRequestQueue(jsonArrayRequest);
     }
 }
