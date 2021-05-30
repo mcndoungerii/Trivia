eb71c1d02cc862285164678ea893809ab41a09fc
mcndoungerii <allenmcndounger@gmail.com>
2021-05-30 18:32:17 +0300

    showing next qns && avoid error ArrayOutOfBoundIndex

--- app/src/main/java/com/ndunga/trivia/MainActivity.java
+++ app/src/main/java/com/ndunga/trivia/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ndunga.trivia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +12,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.ndunga.trivia.controller.AppController;
 import com.ndunga.trivia.data.Repository;
+import com.ndunga.trivia.databinding.ActivityMainBinding;
 import com.ndunga.trivia.model.Question;
 
 import org.json.JSONArray;
@@ -21,16 +23,52 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
 
+    private ActivityMainBinding binding;
+
+    private int currentQuestionIndex = 0;
+
+    private Question quest = new Question();
+
+    List<Question> questionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        List<Question> question = new Repository().getQuestion( questionArrayList -> {
-            Log.d("Question::::", String.valueOf(questionArrayList));
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        binding.setQuestion(quest);
+
+        questionList = new Repository().getQuestion( questionArrayList -> {
+
+
+            binding.questionTextView.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
+        });
+
+        binding.buttonNext.setOnClickListener(view -> {
+            currentQuestionIndex = (currentQuestionIndex + 1) % questionList.size();
+
+            //% questionList.size() -- helps us to avoid the error ArrayOutofBoundIndex
+
+            updateQuestion();
+
+        });
+
+        binding.trueButton.setOnClickListener(view -> {
+
+        });
+
+        binding.falseButton.setOnClickListener(view -> {
+
         });
 
 
+    }
+
+    private void updateQuestion() {
+
+        String question = questionList.get(currentQuestionIndex).getAnswer();
+
+        binding.questionTextView.setText(question);
     }
 }
